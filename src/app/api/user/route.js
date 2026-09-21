d8c0276dbb44611556825b8395984436b0ed86c8
DILRUKSHI T.M.K.A <165579937+kavindiarunika@users.noreply.github.com>
2026-09-21 11:55:19 +0530

    Implement user API with GET and POST methods for user retrieval and creation

---
+++ src/app/api/user/route.js
@@ -0,0 +1,120 @@
+import db from '@/lib/db';
+
+
+export async function GET(req) {
+
+// GET - Get all users
+    try{
+
+        const [users] =await db.query(
+                `
+                 SELECT
+        user_id,
+        first_name,
+        last_name,
+        email,
+        phone,
+        whatsapp_number,
+        postal_code,
+        address_line1,
+        address_line2,
+        city,
+        district,
+        role,
+        status,
+        created_at,
+        updated_at
+      FROM users
+      ORDER BY user_id DESC
+                
+                
+                `
+        )
+
+        return response.json(
+            {
+                success:true,
+                data:users
+            }
+        )
+
+
+
+    }
+    catch(error){
+
+        return response.json(
+            {
+                success:false,
+                message:error.message
+            }
+        )
+
+    }
+
+
+  
+
+    
+
+}
+
+
+  //add new user
+
+  export async function POST(req){
+
+    try{
+         const body =await req.json();
+
+         const {
+      first_name,
+      last_name,
+      email,
+      password,
+      phone,
+      whatsapp_number,
+      postal_code,
+      address_line1,
+      address_line2,
+      city,
+      district,
+    } = body;
+
+    if(!email || !password ){
+
+        return response.json(
+        {
+          success: false,
+          message: "First name, email and password are required",
+        },
+        { status: 400 }
+
+        )
+    }
+
+    //check the email already exists
+
+    const [existingUser] =await db.query(
+
+        "select user_id from users where email = ?",
+        [email]
+    )
+
+    if(existingUser.length >0){
+
+        return response.json(
+            {
+                success:false,
+                message:"Email already exists"
+            }
+        )
+    }
+
+    }
+
+
+    catch(error){
+
+    }
+  }
