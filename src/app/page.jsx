5e1f75c274467ec580340488ddadc3ecd6a176cf
Chathuni <156055327+chathuniimasha@users.noreply.github.com>
2026-09-22 12:36:16 +0530

    refactor: simplify HomePage to redirect to admin

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,5 @@
-import HeroBanner from "../components/user/home/HeroBanner";
-import CategorySection from "../components/user/home/CategorySection";
-import FeaturedProducts from "../components/user/home/FeaturedProducts";
-import NewArrivals from "../components/user/home/NewArrivals";
-import PromoBanner from "../components/user/home/PromoBanner";
-import Navbar from "../components/user/common/Navbar";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-
-  return (
-    <main className="min-h-screen bg-[#EFE9E1]">
-      <Navbar />
-      <HeroBanner />
-      <CategorySection />
-      <FeaturedProducts />
-      <NewArrivals />
-      <PromoBanner />
-      
-
-    </main>
-  );
+export default function HomePage() {
+  redirect("/admin");
 }
