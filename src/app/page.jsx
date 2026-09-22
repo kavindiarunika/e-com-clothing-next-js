9e7647d2a8056b9fa3f5480f6388b1599997e986
DILRUKSHI T.M.K.A <165579937+kavindiarunika@users.noreply.github.com>
2026-09-22 09:38:19 +0530

    refactor: update Home component structure and remove redirect

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,22 @@
-import { redirect } from "next/navigation";
+import HeroBanner from "../components/user/home/HeroBanner";
+import CategorySection from "../components/user/home/CategorySection";
+import FeaturedProducts from "../components/user/home/FeaturedProducts";
+import NewArrivals from "../components/user/home/NewArrivals";
+import PromoBanner from "../components/user/home/PromoBanner";
+import Navbar from "../components/user/common/Navbar";
 
-export default function HomePage() {
-  redirect("/admin");
+export default function Home() {
+
+  return (
+    <main className="min-h-screen bg-[#EFE9E1]">
+      <Navbar />
+      <HeroBanner />
+      <CategorySection />
+      <FeaturedProducts />
+      <NewArrivals />
+      <PromoBanner />
+      
+
+    </main>
+  );
 }
